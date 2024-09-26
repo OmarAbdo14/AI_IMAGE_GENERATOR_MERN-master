f7009f7356e8c1a38b5ffd047adaea227809c6ee
Omar Abdel-rahman <omar.fawzy.19@h-eng.helwan.edu.eg>
2024-09-26 04:51:49 +0300

    a

--- server/index.js
+++ server/index.js
@@ -10,12 +10,12 @@ dotenv.config();
 const app = express();
 app.use(cors());
 app.use(express.json({ limit: "50mb" }));
-app.use(express.urlencoded({ extended: true })); // for form data
+app.use(express.urlencoded({ extended: true }));
 
 app.use("/api/generateImage/", generateImageRoute);
 app.use("/api/post/", posts);
 
-// error handler
+// Error handler
 app.use((err, req, res, next) => {
   const status = err.status || 500;
   const message = err.message || "Something went wrong";
@@ -26,30 +26,19 @@ app.use((err, req, res, next) => {
   });
 });
 
-app.get("/", async (req, res) => {
-  res.status(200).json({
-    message: "Hello developers from GFG",
-  });
-});
-
+// Connect to MongoDB
 const connectDB = () => {
   mongoose.set("strictQuery", true);
   mongoose
     .connect(process.env.MONGODB_URL)
     .then(() => console.log("Connected to Mongo DB"))
     .catch((err) => {
-      console.error("failed to connect with mongo");
+      console.error("Failed to connect with MongoDB");
       console.error(err);
     });
 };
 
-const startServer = async () => {
-  try {
-    connectDB();
-    app.listen(8080, () => console.log("Server started on port 8080"));
-  } catch (error) {
-    console.log(error);
-  }
-};
+connectDB(); // Connect to the database
 
-startServer();
+// Export the Express app for Vercel
+export default app;
