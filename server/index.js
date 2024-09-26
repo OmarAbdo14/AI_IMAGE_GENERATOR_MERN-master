fe670565ee7974a82d24e6f7b963738cf16eaf9d
Omar Abdel-rahman <omar.fawzy.19@h-eng.helwan.edu.eg>
2024-09-26 04:59:20 +0300

    Update index.js

--- server/index.js
+++ server/index.js
@@ -12,6 +12,12 @@ app.use(cors());
 app.use(express.json({ limit: "50mb" }));
 app.use(express.urlencoded({ extended: true }));
 
+app.get("/", (req, res) => {
+  return res.json({
+    success: true,
+    message: "Welcome to the AI Image Generator API",
+  });
+});
 app.use("/api/generateImage", generateImageRoute);
 app.use("/api/post", posts);
 
