c9e1428c4721ab6539cf8dc3120a2eea134b6d46
Omar Abdel-rahman <omar.fawzy.19@h-eng.helwan.edu.eg>
2024-09-26 04:53:38 +0300

    Update index.js

--- server/index.js
+++ server/index.js
@@ -12,8 +12,8 @@ app.use(cors());
 app.use(express.json({ limit: "50mb" }));
 app.use(express.urlencoded({ extended: true }));
 
-app.use("/api/generateImage/", generateImageRoute);
-app.use("/api/post/", posts);
+app.use("/api/generateImage", generateImageRoute);
+app.use("/api/post", posts);
 
 // Error handler
 app.use((err, req, res, next) => {
